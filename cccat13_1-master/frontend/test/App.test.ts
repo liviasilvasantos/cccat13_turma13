bf9fe17ac956e5a5b3c388c0b841accfc452c63e
Livia Silva Santos <liviasilvasantos@gmail.com>
2023-10-17 08:28:15 -0300

    adicionando injeção de dependência

--- cccat13_1-master/frontend/test/App.test.ts
+++ cccat13_1-master/frontend/test/App.test.ts
@@ -1,6 +1,7 @@
 import { mount } from "@vue/test-utils";
 import AppVue from "../src/App.vue";
 import RideGatewayHttp from '../src/infra/gateway/RideGatewayHttp';
+import RideGateway from "../src/infra/gateway/RideGateway";
 
 function sleep (time: number) {
 	return new Promise((resolve) => {
@@ -89,4 +90,55 @@ test("não deve criar um passageiro se email ja cadastrado", async function () {
     await wrapper.get(".signup-submit").trigger("click");
     await sleep(200);
     expect(wrapper.get(".signup-error").text()).toBe("Account already exists");
+});
+
+test("deve criar um passageiro usando fake", async function () {
+    const rideGateway: RideGateway = {
+        async signup(input:any): Promise<any>{
+            return {
+                accountId: crypto.randomUUID()
+            }
+        }
+    }
+    const wrapper = mount(AppVue, {
+        global: {
+            provide: { 
+                rideGateway
+            }
+        }
+    });
+    expect(wrapper.get(".signup-title").text()).toBe("Signup");
+
+    wrapper.get(".signup-name").setValue("John Doe");
+    wrapper.get(".signup-email").setValue(`john.doe${Math.random()}@gmail.com`);
+    wrapper.get(".signup-cpf").setValue("95818705552");
+    wrapper.get(".signup-is-passenger").setValue(true);
+
+    await wrapper.get(".signup-submit").trigger("click");
+    await sleep(200);
+    expect(wrapper.get(".signup-account-id").text()).toHaveLength(36);
+});
+
+test.only("não deve criar um passageiro se nome invalido usando fake", async function () {
+    const rideGateway: RideGateway = {
+        async signup(input:any): Promise<any>{
+            throw new Error("Invalid name")
+        }
+    }
+    const wrapper = mount(AppVue, {
+        global: {
+            provide: { 
+                rideGateway
+            }
+        }
+    });
+    expect(wrapper.get(".signup-title").text()).toBe("Signup");
+
+    wrapper.get(".signup-email").setValue(`john.doe${Math.random()}@gmail.com`);
+    wrapper.get(".signup-cpf").setValue("12312312312");
+    wrapper.get(".signup-is-passenger").setValue(true);
+
+    await wrapper.get(".signup-submit").trigger("click");
+    await sleep(200);
+    expect(wrapper.get(".signup-error").text()).toBe("Invalid name");
 });
